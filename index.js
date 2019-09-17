7aedd5a1105c03ae1cce7f428430d74f7e0f1df4
khalis afkari <khalis411@gmail.com>
2019-09-17 18:05:42 +0700

    p

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import App from './App';
 import { name as appName } from './app.json';
 import firebase from 'react-native-firebase'
 
-firebase.admob.initialize('ca-app-pub-8637010206853096~2004193803');
+firebase.admob().initialize('ca-app-pub-8637010206853096~2004193803');
 AppRegistry.registerComponent(appName, () => App);
