e952cd4d1b526eb810095c6d6070ec52b61ec3bc
khalis afkari <khalis411@gmail.com>
2019-09-18 17:18:02 +0700

    com

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import { name as appName } from './app.json';
 // import firebase from 'react-native-firebase'
 import OneSignal from 'react-native-onesignal'; 
 
-
 // firebase.admob().initialize('ca-app-pub-8637010206853096~2004193803');
 OneSignal.init('afed054c-0e68-4952-8e00-4f51e982e83a');
 
