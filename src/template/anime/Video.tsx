c0ef57964606bae2c7daeca9edeee72516fc857c
khalis afkari <khalis411@gmail.com>
2019-09-29 19:25:27 +0700

    d

--- src/template/anime/Video.tsx
+++ src/template/anime/Video.tsx
@@ -1,47 +1,45 @@
 import React, { useEffect, useState } from 'react'
-import { View } from 'react-native';
-import {useNavigation} from 'react-navigation-hooks'
+import { View, ActivityIndicator } from 'react-native';
 import anime from '../../utils/anime';
 import IVideo from 'react-native-ivideo';
 
-const VideoAnime = () => {
+class VideoAnime extends React.Component<any,any>{
 
-    const [video,setVideo] = useState('')
-    const [load,setLoad] = useState(true)
-    const navigation = useNavigation()
+    state = {
+        loading:true,
+        video:''
+    }
 
-    useEffect(()=>{
-       const id =  navigation.getParam('id')
-       anime.Video(id).then(res=>{
-           console.log(res);
-           setLoad(false)
-           //@ts-ignore
-           setVideo(res)
-       })
-    },[])
-    
+    async componentDidMount(){
+        const data = await anime.Video(this.props.navigation.getParam('id'))
+        this.setState({
+            loading:false,
+            video:data
+        })
+    }
 
-    return load ? (
-        <View style={{flex:1}}>
-            <IVideo
-                source={{
-                    uri:video
-                }}
-                title=""
-                showFullscreenIcon={true}
-                width='100%'
-                height={240}                
-            />
-        </View>
-    ):(
-        <View style={{flex:1}}>
-            <video width="100%" height="100%" controls>
-                <source src={video}/>
-            </video>
-        </View>
-    )
-}   
 
-VideoAnime.navigationOptions = {header:null}
+    render(){
+        if(this.state.loading){
+            return <View style={{flex:1,justifyContent:'center',alignItems:'center'}}><ActivityIndicator/></View>
+        }
+        return(
+            <View style={{flex:1}}>
+                <IVideo
+                    source={{
+                        uri:this.state.video
+                    }}
+                    width='100%'
+                    height={240}
+                    startFullscreen={true}   
+                />
+            </View>
+        )
+    }
+
+    static navigationOptions = {header:null}
+
+}
+
 
 export default VideoAnime
