05b00674eadfef6c21d9ee1af57b2eaeef4ea9b0
khalis afkari <khalis411@gmail.com>
2019-09-30 20:18:57 +0700

    p

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-// import firebase from 'react-native-firebase'
-// import OneSignal from 'react-native-onesignal'; 
+import firebase from 'react-native-firebase'
+import OneSignal from 'react-native-onesignal'; 
 
-// firebase.admob().initialize('ca-app-pub-8637010206853096~2004193803');
-// OneSignal.init('afed054c-0e68-4952-8e00-4f51e982e83a');
+firebase.admob().initialize('ca-app-pub-8637010206853096~2004193803');
+OneSignal.init('afed054c-0e68-4952-8e00-4f51e982e83a');
 
-console.disableYellowBox = true
+// console.disableYellowBox = true
 AppRegistry.registerComponent(appName, () => App);
